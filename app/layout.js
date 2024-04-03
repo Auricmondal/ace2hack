2ce8fa0a2f32fd966aa9c19d34008570c74962dd
Auricmondal <auricmondal@email.com>
2024-04-03 17:56:52 +0530

    basic ui update 1.2

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar/Navbar";
-import Modal from "./components/Modals/Modal";
 import { ClerkProvider } from '@clerk/nextjs'
-import SignInModal from "./components/Modals/SIgnInModal";
+import SignInModal from "./components/Modals/SignInModal";
 import SignUpModal from "./components/Modals/SignUpModal";
 import SideMenu from "./components/Navbar/SideMenu";
 
